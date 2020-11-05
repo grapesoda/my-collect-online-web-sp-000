58e9d19b9f0ba915257b7226412e1fba99db19ca
Tres Tarwater <trest03@gmail.com>
2020-11-05 16:17:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,15 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 def my_collect
-  
+  def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
   
   
 end
